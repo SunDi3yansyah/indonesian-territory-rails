76961fd3498e2e2fe44bb7735d9e47f8782046c9
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-04-13 19:53:12 +0700

    coming soon for csv

--- db/seed/indonesian_territory_csv.rb
+++ db/seed/indonesian_territory_csv.rb
@@ -1,8 +1,1 @@
-# TODO:
-# - ruby script to import csv
-# - update for timestamp
-#
-#   Province.update_all(created_at: DateTime.now, updated_at: DateTime.now)
-#   Regency.update_all(created_at: DateTime.now, updated_at: DateTime.now)
-#   District.update_all(created_at: DateTime.now, updated_at: DateTime.now)
-#   Village.update_all(created_at: DateTime.now, updated_at: DateTime.now)
+abort "Import CSV file is coming soon!"
